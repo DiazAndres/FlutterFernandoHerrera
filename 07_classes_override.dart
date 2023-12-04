72290f7775f489996e1fc162e9b631aba207d334
Andres Diaz <heladio476@gmail.com>
2023-12-03 22:21:29 -0600

    07_classes_override

---
+++ 07_classes_override.dart
@@ -0,0 +1,14 @@
+void main (){
+  final Hero wolverine = Hero(name: 'Logan', power: 'Regeneración');
+  print(wolverine.name);
+  print(wolverine.power);
+}
+
+
+class Hero{
+  
+  String name;
+  String power;
+  
+  Hero({required this.name, required this.power,});
+}
