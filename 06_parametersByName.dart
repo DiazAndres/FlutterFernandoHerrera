02b9e64cc3b664532fc4ea96a0dbf78b5b77fe1e
Andres Diaz <heladio476@gmail.com>
2023-12-03 22:04:54 -0600

    06_parametersByName

---
+++ 06_parametersByName.dart
@@ -0,0 +1,22 @@
+void main(){
+  print(greetEveryone());
+
+  print('Suma ${addTwoNumbers(18,20)}');
+  
+  print(greetPerson(name: 'Andrés', message: 'Hi,'));
+}
+
+
+String greetEveryone() => 'Hello everyone!';
+
+
+int addTwoNumbers(int a, int b) => a + b;
+
+//Parámetro de forma opcional va con [ ]
+int addTwoNumbersOptional(int a, [int b = 0]) => a + b;
+
+//{} se utilizan para hacer los parametros opcionales
+//required para hacer obligatorio el parametro
+String greetPerson( { required String name, String message = 'Hola,'}){
+  return '$message $name';
+}
