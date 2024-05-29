d08ec19ef7b42a5587cf08a2dab16366fb6da37c
Andres Diaz <heladio476@gmail.com>
2024-05-29 08:08:08 -0600

    End TokTik without videos (assets)

--- toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
+++ toktik/lib/presentation/widgets/shared/video_scrollable_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 import 'package:toktik/domain/entities/video_post.dart';
 import 'package:toktik/presentation/widgets/shared/video_player/video_buttons.dart';
+import 'package:toktik/presentation/widgets/video/fullscreen_player.dart';
 
 class VideoScrollableView extends StatelessWidget {
   final List<VideoPost> videos;
@@ -19,7 +20,12 @@ class VideoScrollableView extends StatelessWidget {
         return Stack(
           children: [
             // Video player + gradiente
-
+            SizedBox.expand(
+              child: FullScreenPlayer(
+                caption: videoPost.caption,
+                videoUrl: videoPost.videoUrl,
+              ),
+            ),
             // Botones
             Positioned(
               bottom: 40,
