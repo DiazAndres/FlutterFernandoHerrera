465dac12fb4ca49cc64d5a6ee6c675ff698ff903
Andres Diaz <heladio476@gmail.com>
2024-07-01 14:17:38 -0600

    Cinemapedia - Part 5 - FutureProvider Family modifier

--- cinemapedia/lib/presentation/providers/providers.dart
+++ cinemapedia/lib/presentation/providers/providers.dart
@@ -1,5 +1,6 @@
 export 'actors/actors_by_movie_provider.dart';
 export 'actors/actors_repository_provider.dart';
+
 export 'movies/initial_loading_provider.dart';
 export 'movies/movie_info_provider.dart';
 export 'movies/movies_providers.dart';
@@ -7,3 +8,5 @@ export 'movies/movies_repository_provider.dart';
 export 'movies/movies_slideshow_provider.dart';
 
 export 'search/saerch_movies_provider.dart';
+
+export 'storage/local_storage_provider.dart';
