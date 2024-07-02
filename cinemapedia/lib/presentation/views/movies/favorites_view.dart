bfc616a2cd36abec487f42ce2d21b55e479f6f22
Andres Diaz <heladio476@gmail.com>
2024-07-02 11:16:13 -0600

    Cinemapedia - Part 5 - StaggeredGridView / MasonryGridView

--- cinemapedia/lib/presentation/views/movies/favorites_view.dart
+++ cinemapedia/lib/presentation/views/movies/favorites_view.dart
@@ -1,17 +1,26 @@
+import 'package:cinemapedia/presentation/providers/providers.dart';
+import 'package:cinemapedia/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class FavoritesView extends StatelessWidget {
+class FavoritesView extends ConsumerStatefulWidget {
   const FavoritesView({super.key});
 
   @override
+  FavoritesViewState createState() => FavoritesViewState();
+}
+
+class FavoritesViewState extends ConsumerState<FavoritesView> {
+  @override
+  void initState() {
+    super.initState();
+    ref.read(favoriteMoviesProvider.notifier).loadNextPage();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Favorites View'),
-      ),
-      body: const Center(
-        child: Text('Favoritos'),
-      ),
-    );
+    final favoriteMovies = ref.watch(favoriteMoviesProvider).values.toList();
+
+    return MovieMasonry(movies: favoriteMovies);
   }
 }
