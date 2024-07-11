8b906cc8e4557d9a772e0c7be31c3724f798e1fb
Andres Diaz <heladio476@gmail.com>
2024-07-11 14:05:34 -0600

    Section 19 - Bloc - Equatable (CubitCounter)

---
+++ forms_app/lib/presentation/screens/bloc_counter_screen.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class BlocCounterScreen extends StatelessWidget {
+  const BlocCounterScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Counter'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.refresh_outlined),
+          ),
+        ],
+      ),
+      body: const Center(
+        child: Text('Counter value: xxx'),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            heroTag: '1',
+            onPressed: () {},
+            child: const Text('+3'),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          FloatingActionButton(
+            heroTag: '2',
+            onPressed: () {},
+            child: const Text('+2'),
+          ),
+          const SizedBox(
+            height: 14,
+          ),
+          FloatingActionButton(
+            heroTag: '3',
+            onPressed: () {},
+            child: const Text('+1'),
+          ),
+        ],
+      ),
+    );
+  }
+}
